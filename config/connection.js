acb0755c8d72c80ea50bd28bd1f4609eeb2eefda
wcotton229 <32826633+wcotton229@users.noreply.github.com>
2018-05-02 21:50:59 -0500

    Added JAWSDB database

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-if (proces.enc.JAWSDB_URL){
+if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
     } else {
         connection = mysql.createConnection({
-            host: 'o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-            user: 'gjr9ed417qckks3k',
-            password:'red87brb73p2cnlb',
-            database: 'zgaknl340nh29v3m'
+            host: 'localhost',
+            user: 'root',
+            password:'password',
+            database: 'burger_db'
         });
     };
 
