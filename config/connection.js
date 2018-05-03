807c3d7804c13851551e0acb813153efca39601e
wcotton229 <32826633+wcotton229@users.noreply.github.com>
2018-05-02 21:58:30 -0500

    Corrected connection files

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
 if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -11,14 +12,6 @@ if (process.env.JAWSDB_URL){
             database: 'burger_db'
         });
     };
-
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "burger_db"
-});
 
 // Make connection.
 connection.connect(function(err) {
