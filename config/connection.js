dab42b98626c95c509149bfd79f43a7ff206cebf
wcotton229 <32826633+wcotton229@users.noreply.github.com>
2018-05-02 20:07:00 -0500

    Added JAWS_DB

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if (proces.enc.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    } else {
+        connection = mysql.createConnection({
+            host: 'o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+            user: 'gjr9ed417qckks3k',
+            password:'red87brb73p2cnlb',
+            database: 'zgaknl340nh29v3m'
+        });
+    };
+
 var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
